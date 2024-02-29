8d775337c446ea374a986aa519b567eb52a13c10
aman-sande <154793951+aman-sande@users.noreply.github.com>
2024-02-29 11:49:54 +0530

    Added clients json file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,13 +14,16 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent implements OnInit{
 
   products:any[]=[];
+  clients:any[]=[];
   constructor(private http:HttpClient){}
   ngOnInit(): void {
 
     this.http.get('assets/Json/products.json').subscribe((data:any)=>{
       this.products=data;
+    }),
+    this.http.get('assets/Json/clients.json').subscribe((Clientdata:any)=>{
+      this.clients=Clientdata;
     });
-    
   }
   homeImage="./assets/image/home-img.png"
 

0f30af12d628a3abe1019f579eab573c76b5b064
aman-sande <154793951+aman-sande@users.noreply.github.com>
2024-02-29 11:34:54 +0530

    Added Json File For Products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import path from 'path';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component:LoginComponent
+
+    },
+    {
+    path: '',
+   component:HomeComponent
+},
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink,LoginComponent,NgFor,CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+
+  products:any[]=[];
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+
+    this.http.get('assets/Json/products.json').subscribe((data:any)=>{
+      this.products=data;
+    });
+    
+  }
+  homeImage="./assets/image/home-img.png"
+
+  car1="./assets/image/car-1.png";
+  car2="./assets/image/car-2.png";
+  car3="./assets/image/car-3.png";
+  car4="./assets/image/car-4.png";
+  car5="./assets/image/car-5.png";
+  car6="./assets/image/car-6.png";
+  car7="./assets/image/car-7.png";
+  car8="./assets/image/car-8.png";
+
+  pic1="./assets/image/pic-1.png";
+  pic2="./assets/image/pic-2.png";
+  pic3="./assets/image/pic-3.png";
+  
+  
+ 
+}
+
